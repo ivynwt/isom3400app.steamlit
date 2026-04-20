b5a8a63d3e32bc69d2c5e78ee3ad09713b946a8e
ivynwt <wtngaf@connect.ust.hk>
2026-04-20 17:41:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.title("Welcome to Streamlit!")
 st.write("Hello, Streamlit!")
 st.write(12345)
 st.write({"Name": "Alice", "Age": 30})
-st.write["list1","list2"]
+st.write(["list1","list2"])
 
 st.write("**Bold Text** and *Italic Text*")
 
