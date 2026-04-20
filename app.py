c8e938051686a7d329222e64cf1fb0f333a514de
ivynwt <wtngaf@connect.ust.hk>
2026-04-20 17:20:58 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 # Title of the app
-st.title("Business Data Dashboard")
+st.title("===Business Data Dashboard===")
 
 # Introduction text
 st.write("This dashboard helps visualize sales data over time.")
