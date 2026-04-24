ffdfdfa5d1f3e83e7da0f706e956317274315201
ivynwt <wtngaf@connect.ust.hk>
2026-04-24 13:08:20 +0800

    Update app.24.4.py

--- app.24.4.py
+++ app.24.4.py
@@ -53,7 +53,7 @@ if st.button("Calculate"):
     elif operation == "Divide":
         result = num1 / num2 if num2 != 0 else "Error: Division by zero"
 
-    st.success(f"Result: {result}")
+    st.success(f"Result: {result:,.0f}")
 
 
 
