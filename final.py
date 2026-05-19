6fc35248c807c648a67856ba2593e441755d0337
ivynwt <wtngaf@connect.ust.hk>
2026-05-19 23:15:12 +0800

    Create final.py

---
+++ final.py
@@ -0,0 +1,7 @@
+st.title("This is title.")
+st.header("This is header.") 
+st.subheader("This is subheader.")
+st.write("This is content written.")
+st.write("**Bold Text** and *Italic Text*")
+st.markdown("**Bold Text - md** and *Italic Text - md*")
+st.success("Operation completed successfully!")
